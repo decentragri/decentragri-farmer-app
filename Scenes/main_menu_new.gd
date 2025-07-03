92189214124e8c3453de0a5db0df142ce9f10502
decentragri <decentragri@gmail.com>
2025-07-03 11:58:05 +0800

    feat: UI V2

---
+++ Scenes/main_menu_new.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+
+
+func _ready() -> void:
+	connect_signals()
+
+
+func connect_signals() -> void:
+	set_bottom_menu_buttons()
+	
+
+func set_bottom_menu_buttons() -> void:
+	for button: TextureButton in get_tree().get_nodes_in_group(&"BottonMenuButtons"):
+		var _1: int = button.pressed.connect(_on_set_botton_menu_buttons_pressed.bind(button.name))
+
+
+func _on_set_botton_menu_buttons_pressed(button_name: String) -> void:
+	for button: TextureButton in get_tree().get_nodes_in_group("BottonMenuButtons"):
+		var panel: Panel = button.get_parent()
+		var icon: TextureRect = button.get_child(0)
+		var is_selected: bool = button.name == button_name
+
+		button.button_pressed = is_selected
+
+		# ✅ Mutate the existing stylebox (assumes per-panel instance)
+		var stylebox: StyleBoxFlat = panel.get_theme_stylebox("panel")
+		if is_selected:
+			stylebox.bg_color = Color("9ff300")
+			icon.modulate = Color("ffffff")
+		else:
+			stylebox.bg_color = Color("1d1d1d")
+			icon.modulate = Color("a2a0a0")
+
+			
