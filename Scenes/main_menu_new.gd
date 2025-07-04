5698c9f03b70846fbd36e094c0a24baad6b29c87
decentragri <decentragri@gmail.com>
2025-07-04 18:04:14 +0800

    feat: created new auth UI

--- Scenes/main_menu_new.gd
+++ Scenes/main_menu_new.gd
@@ -10,15 +10,17 @@ func _ready() -> void:
 func connect_signals() -> void:
 	set_bottom_menu_buttons()
 	
-
+	
 func set_bottom_menu_buttons() -> void:
 	for button: TextureButton in get_tree().get_nodes_in_group(&"BottonMenuButtons"):
 		var _1: int = button.pressed.connect(_on_set_botton_menu_buttons_pressed.bind(button.name))
-
-
+		var _2: int = button.pressed.connect(_on_toggle_container_visibility.bind(button.name))
+	
+	
 func _on_set_botton_menu_buttons_pressed(button_name: String) -> void:
 	for button: TextureButton in get_tree().get_nodes_in_group("BottonMenuButtons"):
 		var panel: Panel = button.get_parent()
+		panel.mouse_filter = Control.MOUSE_FILTER_STOP
 		var icon: TextureRect = button.get_child(0)
 		var is_selected: bool = button.name == button_name
 
@@ -34,3 +36,13 @@ func _on_set_botton_menu_buttons_pressed(button_name: String) -> void:
 			icon.modulate = Color("a2a0a0")
 
 			
+func _on_toggle_container_visibility(button_name: String) -> void:
+	for container: VBoxContainer in get_tree().get_nodes_in_group(&"MainContainers"):
+		if button_name + "Container" == container.name:
+			container.visible = true
+		else:
+			container.visible = false
+	
+	
+
+	
